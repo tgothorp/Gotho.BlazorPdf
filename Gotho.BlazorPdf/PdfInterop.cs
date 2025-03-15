69514ed8279f11ae5db70056fe0ace3e8379cd07
tgothorp <tgothorp@users.noreply.github.com>
2025-03-15 14:33:10 +0000

    Fix scroll mode bug

--- Gotho.BlazorPdf/PdfInterop.cs
+++ Gotho.BlazorPdf/PdfInterop.cs
@@ -4,7 +4,7 @@ using Microsoft.JSInterop;
 internal class PdfInterop(IJSRuntime jsRuntime) : IAsyncDisposable
 {
     private readonly Lazy<Task<IJSObjectReference>> js =
-        new(() => jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/Gotho.BlazorPdf/mudpdf.min.js").AsTask());
+        new(() => jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/Gotho.BlazorPdf/blazorpdf.min.js").AsTask());
 
     public async Task InitializeAsync(object objRef, Pdf pdf, bool singlePageMode)
     {
@@ -17,66 +17,18 @@ internal class PdfInterop(IJSRuntime jsRuntime) : IAsyncDisposable
         var module = await js.Value;
         await module.InvokeVoidAsync("updatePdf", objRef, pdf.GetPdfState());
     }
-    
+
     public async Task PrintDocumentAsync(object objRef, Pdf pdf)
     {
         var module = await js.Value;
         await module.InvokeVoidAsync("printDocument", objRef, pdf.Id);
     }
-    
+
     public async Task DownloadDocumentAsync(object objRef, Pdf pdf)
     {
         var module = await js.Value;
         await module.InvokeVoidAsync("downloadDocument", objRef, pdf.Id);
     }
-    
-    // public async Task InitializeAsync(object objRef, string elementId, string documentUrl, double scale, double rotation, bool singlePageMode, string? password = null)
-    // {
-    //     var module = await js.Value;
-    //     await module.InvokeVoidAsync("init", objRef, elementId, documentUrl, scale, rotation, singlePageMode, password);
-    // }
-    //
-    // public async Task FirstPageAsync(object objRef, string elementId)
-    // {
-    //     var module = await js.Value;
-    //     await module.InvokeVoidAsync("firstPage", objRef, elementId);
-    // }
-    //
-    // public async Task LastPageAsync(object objRef, string elementId)
-    // {
-    //     var module = await js.Value;
-    //     await module.InvokeVoidAsync("lastPage", objRef, elementId);
-    // }
-    //
-    // public async Task PreviousPageAsync(object objRef, string elementId)
-    // {
-    //     var module = await js.Value;
-    //     await module.InvokeVoidAsync("previousPage", objRef, elementId);
-    // }
-    //
-    // public async Task NextPageAsync(object objRef, string elementId)
-    // {
-    //     var module = await js.Value;
-    //     await module.InvokeVoidAsync("nextPage", objRef, elementId);
-    // }
-    //
-    // public async Task GotoPageAsync(object objRef, string elementId, int gotoPageNum)
-    // {
-    //     var module = await js.Value;
-    //     await module.InvokeVoidAsync("goToPage", objRef, elementId, gotoPageNum);
-    // }
-    //
-    // public async Task ZoomInOutAsync(object objRef, string elementId, double scale)
-    // {
-    //     var module = await js.Value;
-    //     await module.InvokeVoidAsync("zoom", objRef, elementId, scale);
-    // }
-    //
-    // public async Task RotateAsync(object objRef, string elementId, double rotation)
-    // {
-    //     var module = await js.Value;
-    //     await module.InvokeVoidAsync("rotate", objRef, elementId, rotation);
-    // }
 
     public async ValueTask DisposeAsync()
     {
