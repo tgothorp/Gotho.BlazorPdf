3d658adef6f47c6faf53c19194aada95af26b9c6
tgothorp <tgothorp@users.noreply.github.com>
2025-03-28 11:06:14 +0000

    Allow color and localisation to be done at runtime

---
+++ Gotho.BlazorPdf/Config/BlazorPdfLocalizedStrings.cs
@@ -0,0 +1,22 @@
+namespace Gotho.BlazorPdf.Config;
+
+/// <summary>
+/// Localized strings for the PDF viewer 
+/// </summary>
+public class BlazorPdfLocalizedStrings
+{
+    public string ToggleThumbnails { get; set; } = "Show Thumbnails";
+    public string PreviousPage { get; set; } = "Previous Page";
+    public string NextPage { get; set; } = "Next Page";
+    public string PageOf { get; set; } = "of";
+    public string ZoomIn { get; set; } = "Zoom In";
+    public string ZoomOut { get; set; } = "Zoom Out";
+    public string RotateClockwise { get; set; } = "Rotate Clockwise";
+    public string RotateCounterclockwise { get; set; } = "Rotate Counterclockwise";
+    public string SwitchOrientation { get; set; } = "Switch Orientation";
+    public string FirstPage { get; set; } = "First Page";
+    public string LastPage { get; set; } = "Last Page";
+    public string ResetZoom { get; set; } = "Reset Zoom";
+    public string PrintDocument { get; set; } = "Print Document";
+    public string DownloadDocument { get; set; } = "Download Document";
+}
