15ded5a27ed0adcf41b2aa8345ae6294d1da4cd1
Tom Gothorp <tgothorp@users.noreply.github.com>
2025-08-02 17:35:56 +0100

    File upload (#58)
    * File upload
    
    * CSS tweak

--- Gotho.BlazorPdf/Config/BlazorPdfConfig.cs
+++ Gotho.BlazorPdf/Config/BlazorPdfConfig.cs
@@ -13,4 +13,23 @@ public class BlazorPdfConfig
     /// <para><b>Default:</b> <c>false</c></para>
     /// </remarks>
     public bool UseProjectWorker { get; set; } = false;
+
+    /// <summary>
+    /// If no <c>URL</c> parameter is specified in the PdfViewer component then a user will be allowed to upload
+    /// a PDF file unless option is set to <c>false</c>
+    /// </summary>
+    /// <remarks>
+    /// As always, you should consider any potential security implications of allowing users to upload their own files
+    ///
+    /// <para><b>Default:</b> <c>true</c></para>
+    /// </remarks>
+    public bool PermitPdfFileUpload { get; set; } = true;
+
+    /// <summary>
+    /// Determines the maximum file size that can be uploaded, used in conjunction with <c>PermitPdfFileUpload</c>
+    /// </summary>
+    /// <remarks>
+    /// <para><b>Default:</b> 10MB (<c>10 * 1024 * 1024</c>)</para>
+    /// </remarks>
+    public long MaxPdfFileUploadSize { get; set; } = 10 * 1024 * 1024;
 }
