cd8b6bfc3a7bf7768decdbf4d60965e2ead4d4de
Andres Soria <devdres@bluewin.ch>
2026-04-13 14:37:27 +0200

    feat: make MudPdfViewer input variants configurable.

--- Gotho.BlazorPdf.MudBlazor/MudPdfViewer.razor.cs
+++ Gotho.BlazorPdf.MudBlazor/MudPdfViewer.razor.cs
@@ -1,5 +1,6 @@
 using Gotho.BlazorPdf.MudBlazor.Config;
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace Gotho.BlazorPdf.MudBlazor;
 
@@ -7,4 +8,7 @@ public partial class MudPdfViewer : PdfViewer
 {
     [Parameter] public MudPdfIconConfig Icons { get; set; } = new();
     [Parameter] public MudPdfColorConfig MudColors { get; set; } = new();
+    [Parameter] public Variant PageFieldVariant { get; set; } = Variant.Filled;
+    [Parameter] public Variant PasswordFieldVariant { get; set; } = Variant.Filled;
+    [Parameter] public Variant SearchFieldVariant { get; set; } = Variant.Filled;
 }
